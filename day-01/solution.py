190bba9177d012e3b04c805e3f0a01eb44ff1b10
Rodrigo Vieira <rodrigo.vieira@gmail.com>
2020-12-01 18:05:25 -0300

    join parts 1 and 2

--- day-01/solution.py
+++ day-01/solution.py
@@ -1,7 +1,11 @@
 with open("input.txt", "r") as input_file:
     input = [int(value) for value in input_file.readlines()]
 
-result = [
+result_part_one = [
+    (a, b, a * b) for a in input for b in input if a > b and a + b == 2020
+]
+
+result_part_two = [
     (a, b, c, a * b * c)
     for a in input
     for b in input
@@ -9,4 +13,5 @@ result = [
     if a > b and b > c and a + b + c == 2020
 ]
 
-print(result)
+print(result_part_one)
+print(result_part_two)
