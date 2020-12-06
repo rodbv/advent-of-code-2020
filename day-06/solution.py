21d91facd1b22a78095b5f4c815c35ae109c1988
Rodrigo Vieira <rodrigo.vieira@gmail.com>
2020-12-06 16:02:56 -0300

    Day 6 done

--- day-06/solution.py
+++ day-06/solution.py
@@ -4,14 +4,13 @@ with open("input.txt", "r") as input_file:
     input = input_file.read().split('\n\n')
 
 def part_one():
-    return(sum([len(set(group.replace('\n', ''))) for group in input]))
+    return(sum([len(set(group)) for group in input]))
     
 def part_two():
     result = 0
     for group in input:
-        sets = 6[set(answer) for answer in group.split('\n') if answer != '']
-        result += len(reduce(set.intersection, sets))
+        result += len(reduce(set.intersection, [set(answer) for answer in group.split()]))
     return result 
     
-# print(part_one())
+print(part_one())
 print(part_two())
